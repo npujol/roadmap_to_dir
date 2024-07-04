01f60f501f43fa0f435674cb45fd2c2753405a8a
npujol <naivy.luna@gmail.com>
2024-07-04 13:07:09 +0200

    add initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Roadmap to directory
+
+This app import a roadmap from [Roadmap Site](https://roadmap.sh/) into a directory structure.
---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Convert roadmap into a directory structure";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication defaultPoetryOverrides;
+      environment-variable = ''
+      '';
+      in
+        with pkgs; rec {
+          devShell = mkShell {
+            name = "roadmap_to_dir";
+            buildInputs = [
+              pkgs.python312
+              pkgs.poetry
+            ];
+            shellHook = ''
+              poetry env use ${pkgs.lib.getExe pkgs.python312}
+              export VIRTUAL_ENV=$(poetry env info --path)
+              export PATH=$VIRTUAL_ENV/bin/:$PATH
+              ${environment-variable}
+            '';
+          };
+
+          # Runtime package with all dependencies using python version as default option.
+          packages.app = mkPoetryApplication {
+            projectDir = ./.;
+            preferWheels = true;
+            python = pkgs.python312;
+            checkGroups = [];
+          };
+
+          # # Use xvfb-run to run the bot in headless mode
+          # packages.bot = pkgs.writeShellScriptBin "bot" ''
+          #   ${environment-variable}
+          #   ${pkgs.lib.getExe pkgs.xvfb-run} ${packages.app}/bin/bot
+          # '';
+
+          # defaultPackage = packages.bot;
+          formatter = pkgs.alejandra;
+        }
+    );
+}
